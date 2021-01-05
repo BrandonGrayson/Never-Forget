42a6c4a83e09cba5805653ffdcf75420916703aa
Brandon <brandonbgrayson@gmail.com>
2021-01-05 11:36:14 -0500

    home page file connected

--- Develop/public/assets/js/server.js
+++
@@ -1,5 +0,0 @@
-// be able to use express to send data back
-var app = require('express')
-
-console.log(app)
-
---
+++ server.js
@@ -0,0 +1,32 @@
+// be able to use express to send data back
+// DEPENDENCIES
+var express = require('express')
+var path = require("path");
+
+// SET up Express app
+// save express to app variable
+var app = express()
+// dynamic Port including HEROKU  
+var PORT = process.env.PORT || 3001;
+
+// Set up Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'))
+
+// Basic route that sends the user to Home page
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "index.html"));
+});
+
+// Route to send user to notes page
+app.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "public/notes.html"));
+});
+
+// console.log(app)
+
+// starts the server to begin listening
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+});
